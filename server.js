73415ffe630b1aee0f94faf046bd04fd2ea172c9
Alex Palx <alex.teanow@gmail.com>
2022-06-19 16:55:51 +0300

    refactor: some refactoring

--- server.js
+++ server.js
@@ -24,132 +24,150 @@ let database = [
 
 const PORT = 5000;
 
+const checkURL = (req, res) => {
+    if (!req.url.match(/\/api\/users\/.+/)) {
+        res.writeHead(404, 'Content-type', 'application/json');
+        res.end(JSON.stringify({ message: 'Invalid URL' }));
+        return;
+    }
+    return true;
+};
+
+const checkId = (req, res) => {
+    const id = req.url.split('/')[3];
+    if (id.length === 0 || !validate(id)) {
+        res.writeHead(400, 'Content-type', 'application/json');
+        res.end(JSON.stringify({ message: 'Invalid userId' }));
+        return;
+    }
+    return true;
+};
+
+const checkUser = (req, res) => {
+    const id = req.url.split('/')[3];
+    const user = database.find(user => user.id === id);
+
+    if (!user) {
+        res.writeHead(404, 'Content-type', 'application/json');
+        res.end(JSON.stringify({ message: 'User doesn\'t exist' }));
+        return;
+    }
+    return true;
+};
+
+const getUsers = (res) => {
+    res.writeHead(200, 'Content-type', 'application/json');
+    res.write(JSON.stringify(database));
+    res.end();
+    return true;
+};
+
+
+const getUser = (req, res) => {
+    const id = req.url.split('/')[3];
+    const user = database.find(user => user.id === id);
+
+    if (user) {
+        res.writeHead(200, 'Content-type', 'application/json');
+        res.end(JSON.stringify(user));
+        return true;
+    }
+    else {
+        res.writeHead(404, 'Content-type', 'application/json');
+        res.end(JSON.stringify({ message: 'User doesn\'t exist' }));
+        return;
+    }
+};
+
 const server = createServer((req, res) => {
-    let body = '';
-    let id;
-    switch (req.method) {
-        case 'GET':
-            // /api/users || /api/users/
-            if (req.url === '/api/users' || req.url === '/api/users/') {
-                res.writeHead(200, 'Content-type', 'application/json');
-                res.write(JSON.stringify(database));
-                res.end();
-                return;
-            }
-            // /api/users/id
-            else if (req.url.match(/\/api\/users\/.+/)) {
-                const id = req.url.split('/')[3];
-                if (id.length === 0 || !validate(id)) {
-                    res.writeHead(400, 'Content-type', 'text/html');
-                    res.end('Incorrect uuid');
+    try {
+        let body = '';
+        switch (req.method) {
+            case 'GET':
+                // /api/users || /api/users/
+                if (req.url === '/api/users' || req.url === '/api/users/') {
+                    getUsers(res);
                     return;
                 }
-
-                const user = database.find(user => user.id === id);
-
-                if (user) {
+                // /api/users/id
+                else if (req.url.match(/\/api\/users\/.+/)) {
+                    if (!checkId(req, res)) return;
+                    if (!getUser(req, res)) return;
+                }
+    
+                checkURL(req, res);
+                break;
+    
+            case 'POST':
+                req.on('data', chunk => {
+                    body += chunk.toString();
+                });
+                req.on('end', () => {
+                    body = JSON.parse(body);
+    
+                    if (!body.username ||
+                        !body.age ||
+                        !body.hobbies ||
+                        !Array.isArray(body.hobbies)
+                    ) {
+                        res.writeHead(400, 'Content-type', 'application/json');
+                        res.end(JSON.stringify({ message: 'Object must have properties: username: string, age: number, hobbies: array' }));
+                        return;
+                    }
+    
+                    body = { ...body, id: uuidv4() };
+                    database.push(body);
+    
+                    res.writeHead(201, 'Content-type', 'application/json');
+                    res.end(JSON.stringify(body));
+                });
+                break;
+    
+            case 'PUT':
+                if (!checkURL(req, res)) return;
+                if (!checkId(req, res)) return;
+                if (!checkUser(req, res)) return;
+    
+                req.on('data', chunk => {
+                    body += chunk.toString();
+                });
+                req.on('end', () => {
+                    body = JSON.parse(body);
+    
+                    const userDatabaseId = database.findIndex(user => user.id === req.url.split('/')[3]);
+    
+                    if (userDatabaseId === -1) {
+                        res.writeHead(404, 'Content-type', 'application/json');
+                        res.end(JSON.stringify({ message: 'User doesn\'t exist' }));
+                        return;
+                    }
+    
+                    database[userDatabaseId] = { ...database[userDatabaseId], ...body, id: database[userDatabaseId].id };
                     res.writeHead(200, 'Content-type', 'application/json');
-                    res.end(JSON.stringify(user));
+                    res.end(JSON.stringify(database[userDatabaseId]));
                     return;
-                }
-                else {
-                    res.writeHead(404, 'Content-type', 'text/html');
-                    res.end(`User ${id} doesn't exist`);
-                    return;
-                }
-            }
-            res.writeHead(404, 'Content-type', 'text/html');
-            res.end('Error 404');
-            break;
-
-        case 'POST':
-            req.on('data', chunk => {
-                body += chunk.toString();
-            });
-            req.on('end', () => {
-                body = JSON.parse(body);
-
-                if (!body.username ||
-                    !body.age ||
-                    !body.hobbies ||
-                    !Array.isArray(body.hobbies)
-                ) {
-                    res.writeHead(400, 'Content-type', 'text/html');
-                    res.end('object must have properties: username: string, age: number, hobbies: array');
-                    return;
-                }
-
-                body = { ...body, id: uuidv4() };
-                database.push(body);
-
-                res.writeHead(201, 'Content-type', 'application/json');
-                res.end(JSON.stringify(body));
-            });
-            break;
-
-        case 'PUT':
-            if (!req.url.match(/\/api\/users\/.+/)) {
-                res.writeHead(404, 'Content-type', 'text/html');
-                res.end('Error 404');
-                return;
-            }
-
-            id = req.url.split('/')[3];
-            if (id.length === 0 || !validate(id)) {
-                res.writeHead(400, 'Content-type', 'application/json');
-                res.end(JSON.stringify({ message: 'Incorrect id' }));
-                return;
-            }
-
-            if (database.findIndex(user => user.id === req.url.split('/')[3]) === -1) {
-                res.writeHead(404, 'Content-type', 'application/json');
-                res.end(JSON.stringify({ message: 'User doesn\'t exist' }));
-                return;
-            }
-
-            req.on('data', chunk => {
-                body += chunk.toString();
-            });
-            req.on('end', () => {
-                body = JSON.parse(body);
-
-                database[userDatabaseId] = { ...database[userDatabaseId], ...body, id: database[userDatabaseId].id };
-                res.writeHead(200, 'Content-type', 'application/json');
-                res.end(JSON.stringify(database[userDatabaseId]));
-                return;
-            });
-            break;
-
-        case 'DELETE':
-            if (!req.url.match(/\/api\/users\/.+/)) {
-                res.writeHead(404, 'Content-type', 'text/html');
-                res.end('Error 404');
-                return;
-            }
-
-            id = req.url.split('/')[3];
-            if (id.length === 0 || !validate(id)) {
-                res.writeHead(400, 'Content-type', 'application/json');
-                res.end(JSON.stringify({ message: 'Incorrect id' }));
-                return;
-            }
-
-            if (database.findIndex(user => user.id === req.url.split('/')[3]) === -1) {
-                res.writeHead(404, 'Content-type', 'application/json');
-                res.end(JSON.stringify({ message: 'User doesn\'t exist' }));
-                return;
-            }
-
-            database = database.filter(user => user.id !== id);
-
-            res.statusCode = 204;
-            res.end();
-            break;
-
-        default:
-            res.writeHead(404, 'Content-type', 'text/html');
-            res.end('Error 404');
-            break;
+                });
+                break;
+    
+            case 'DELETE':
+                if (!checkURL(req, res)) return;
+                if (!checkId(req, res)) return;
+                if (!checkUser(req, res)) return;
+    
+                database = database.filter(user => user.id !== req.url.split('/')[3]);
+    
+                res.statusCode = 204;
+                res.end();
+                break;
+    
+            default:
+                checkURL(req, res);
+                break;
+        }
+    }
+    catch {
+        res.writeHead(500, 'Content-type', 'application/json');
+        res.end(JSON.stringify({ message: 'Server error' }));
     }
 });
 
